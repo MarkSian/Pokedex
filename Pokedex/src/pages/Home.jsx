dbe1b3bf0a4c88b6b3f7ac1fa6c960a177a60b63
MarkSian <merksianipar@gmail.com>
2025-05-19 14:56:24 -0600

    images added to assets
    imported to generations objects

--- Pokedex/src/pages/Home.jsx
+++ Pokedex/src/pages/Home.jsx
@@ -9,7 +9,10 @@ export default function Home() {
             <h1 className="text-4xl font-bold text-center mb-8">Pokédex Generations</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">WOmp</div>
 
+
+
         </div>
+
 
 
     )
--- Pokedex/src/utils/pokemonData.js
+++ Pokedex/src/utils/pokemonData.js
@@ -1,13 +1,23 @@
+import Gen1PkD from '../assets/pokedex-images/Pokedex-Gen-1.png';
+import Gen2PkD from '../assets/pokedex-images/Pokedex-Gen-2.png';
+import Gen3PkD from '../assets/pokedex-images/Pokedex-Gen-3.png';
+import Gen4PkD from '../assets/pokedex-images/Pokedex-Gen-4.png';
+import Gen5PkD from '../assets/pokedex-images/Pokedex-Gen-5.png';
+import Gen6PkD from '../assets/pokedex-images/Pokedex-Gen-6.png';
+import Gen7PkD from '../assets/pokedex-images/Pokedex-Gen-7.png';
+import Gen8PkD from '../assets/pokedex-images/Pokedex-Gen-8.png';
+import Gen9PkD from '../assets/pokedex-images/Pokedex-Gen-9.png';
+
 export const generations = [
-    { id: 1, name: 'Generation I', limit: 151, offset: 0 },
-    { id: 2, name: 'Generation II', limit: 100, offset: 151 },
-    { id: 3, name: 'Generation III', limit: 135, offset: 251 },
-    { id: 4, name: 'Generation IV', limit: 107, offset: 386 },
-    { id: 5, name: 'Generation V', limit: 156, offset: 493 },
-    { id: 6, name: 'Generation VI', limit: 72, offset: 649 },
-    { id: 7, name: 'Generation VII', limit: 88, offset: 721 },
-    { id: 8, name: 'Generation VIII', limit: 96, offset: 809 },
-    { id: 9, name: 'Generation IX', limit: 120, offset: 905 }
+    { id: 1, name: 'Generation I', limit: 151, offset: 0, image: Gen1PkD },
+    { id: 2, name: 'Generation II', limit: 100, offset: 151, image: Gen2PkD },
+    { id: 3, name: 'Generation III', limit: 135, offset: 251, image: Gen3PkD },
+    { id: 4, name: 'Generation IV', limit: 107, offset: 386, image: Gen4PkD },
+    { id: 5, name: 'Generation V', limit: 156, offset: 493, image: Gen5PkD },
+    { id: 6, name: 'Generation VI', limit: 72, offset: 649, image: Gen6PkD },
+    { id: 7, name: 'Generation VII', limit: 88, offset: 721, image: Gen7PkD },
+    { id: 8, name: 'Generation VIII', limit: 96, offset: 809, image: Gen8PkD },
+    { id: 9, name: 'Generation IX', limit: 120, offset: 905, image: Gen9PkD }
 ];
 
 // offset: will tell us where to start fetching data from
