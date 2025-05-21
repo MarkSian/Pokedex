d8fd1f7847fcf19bd9eee58236bf935a153ecdd2
MarkSian <merksianipar@gmail.com>
2025-05-20 22:01:08 -0600

    added dynamic coloring

--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,5 +1,26 @@
 import { Link } from "react-router-dom";
 
+const typeColors = {
+    normal: "bg-gray-400",
+    fire: "bg-red-500",
+    water: "bg-blue-500",
+    grass: "bg-green-500",
+    electric: "bg-yellow-400 text-black",
+    ice: "bg-cyan-200 text-black",
+    fighting: "bg-orange-700",
+    poison: "bg-purple-500",
+    ground: "bg-yellow-700",
+    flying: "bg-indigo-200 text-black",
+    psychic: "bg-pink-500",
+    bug: "bg-lime-500",
+    rock: "bg-yellow-800",
+    ghost: "bg-indigo-800 text-white",
+    dragon: "bg-indigo-700 text-white",
+    dark: "bg-gray-800 text-white",
+    steel: "bg-gray-500",
+    fairy: "bg-pink-300 text-black",
+};
+
 export default function PokemonGrid({ pokemon }) {
     return (
         <Link to={`/pokemon/${pokemon.id}`} key={pokemon.id}>
@@ -17,7 +38,12 @@ export default function PokemonGrid({ pokemon }) {
                     <p>#{pokemon.id.toString().padStart(3, '0')}</p>
                     <div className="flex gap-2">
                         {pokemon.types.map(type => (
-                            <span key={type} className="badge badge-primary capitalize">{type}</span>
+                            <span
+                                key={type}
+                                className={`badge capitalize ${typeColors[type] || "bg-gray-300"}`}
+                            >
+                                {type}
+                            </span>
                         ))}
                     </div>
                 </div>
